9e9c36fc5e7752093672114d56390877c5253b12
David <ack.dav@gmail.com>
2019-08-11 13:54:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from distutils.core import setup
 setup(
     name='phuzzer', version='8.19.4.30.pre2', description="Python wrapper for multiarch AFL",
     packages=['phuzzer', 'phuzzer.extensions', 'phuzzer.phuzzers'],
-    install_requires=['angr', 'shellphish-qemu', 'shellphish-afl', 'tqdm']
+    install_requires=['angr', 'shellphish-qemu', 'tqdm'],
+    dependency_links=['git+https://github.com/derdav3/shellphish-afl#egg=package-1.0']
 )
